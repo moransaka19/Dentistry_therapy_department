f631d938b3bb92e40008f314ef7ae8cb89fc2632
moransaka19 <moransaka19@gmail.com>
2019-11-10 23:13:34 +0200

    123
    Initialize

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dentistry_therapy_department.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
