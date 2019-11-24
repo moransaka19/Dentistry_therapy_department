64979bf3c32131e784f3a857ccd8e092ec24168f
moransaka19 <moransaka19@gmail.com>
2019-11-25 01:43:02 +0200

    connect to db and unable EF

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,24 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Dentistry_therapy_department.Models;
 
 namespace Dentistry_therapy_department.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private JournalContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(JournalContext context)
         {
-            _logger = logger;
+            db = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await db.Journal.ToListAsync());
         }
-
+       
         public IActionResult Privacy()
         {
             return View();
