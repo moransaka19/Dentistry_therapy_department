af842790f7de762accc53b11299775b5be25dd32
moransaka19 <moransaka19@gmail.com>
2020-02-14 17:37:11 +0200

    Stable Commit

--- DentistryDatabase/dbo/Tables/ProcedureMedicine.sql
+++ DentistryDatabase/dbo/Tables/ProcedureMedicine.sql
@@ -2,8 +2,11 @@
     [MedicineId]  INT NOT NULL,
     [ProcedureId] INT NOT NULL,
     [Count]       INT NULL,
+    CONSTRAINT [MedicineId_PK] PRIMARY KEY CLUSTERED ([MedicineId] ASC, [ProcedureId] ASC),
     FOREIGN KEY ([MedicineId]) REFERENCES [dbo].[Medicine] ([MedicineId]),
     FOREIGN KEY ([ProcedureId]) REFERENCES [dbo].[Procedure] ([ProcedureId]),
     UNIQUE NONCLUSTERED ([MedicineId] ASC, [ProcedureId] ASC)
 );
+
+
 
